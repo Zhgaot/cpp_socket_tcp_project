c1d8de1fb724cc283d856767fdd16d3e21b788bb
zhgaot <zhgaot@163.com>
2021-07-03 17:22:04 +0800

    Modified the specified problem of the compiler, it can be realized that anyone can run directly after cloning(clone) the project.

---
+++ build/CMakeFiles/mySocket_exe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mySocket_exe.dir/main.cpp.o"
+  "CMakeFiles/mySocket_exe.dir/main.cpp.o.d"
+  "mySocket_exe"
+  "mySocket_exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mySocket_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

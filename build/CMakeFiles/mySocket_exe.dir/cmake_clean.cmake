5a2c4197ed334139a7ca04ce1433e6a57886aac9
Your Name <you@example.com>
2021-07-05 20:43:14 +0800

    Modified VSCode's error report about C++11 syntax.

---
+++ build/CMakeFiles/mySocket_exe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mySocket_exe.dir/main.cpp.o"
+  "mySocket_exe.pdb"
+  "mySocket_exe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mySocket_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
